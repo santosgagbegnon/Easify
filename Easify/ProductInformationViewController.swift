eb34ec9ab32aab1f70af9fb6c4a350182addd088
Santos <santos@Santos-MacBook-Pro.local>
2018-09-17 03:06:01 -0400

    Added some transitions, continued improvements on product information page.

--- Easify/ProductInformationViewController.swift
+++ Easify/ProductInformationViewController.swift
@@ -13,34 +13,91 @@ class ProductInformationViewController : UIViewController {
     @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var productInformationTextView: UITextView!
     @IBOutlet weak var productNameLabel: UILabel!
-    
+    @IBOutlet weak var productInformationView: UIView!
+    @IBOutlet  var productInfoViewCenterToSafeAreaY: NSLayoutConstraint!
+    @IBOutlet  var productInfoViewCenterToSafeAreaX: NSLayoutConstraint!
+    @IBOutlet  var productInfoViewEqualHeightToSuperView: NSLayoutConstraint!
+    @IBOutlet  var productInfoViewEqualWidthToSuperView: NSLayoutConstraint!
+    public var cell : ProductCollectionViewCell!
     public var productImage : UIImage! //TO DO: Add default image
     public var product : Product!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        toggleProductInfoViewConstraints(disableConstraints: true)
+        productInformationView.frame = cell.convert(cell.bounds,to:productInformationView)
+        productInformationView.layoutIfNeeded()
+        productNameLabel.alpha = 0
+        productImageView.alpha = 0
+        productInformationTextView.alpha = 0
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0)
+        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [.curveEaseOut], animations: {
+            self.toggleProductInfoViewConstraints(disableConstraints: false)
+        }, completion: {(_) in
+            UIView.animate(withDuration: 0.4, animations: {
+                self.productImageView.alpha = 1
+                self.productNameLabel.alpha = 1
+                self.productInformationTextView.alpha = 1
+            }, completion: { (_) in
+                print("setting text")
+            })
+        })
+    }
     override func viewDidLoad() {
         navigationController?.navigationBar.isHidden = true
         productImageView.image = productImage
         productNameLabel.text = product.title
-        productInformationTextView.text = text()
+        productInformationTextView.attributedText = text()
+
     }
-    private func text() -> String{
-//        -body_html: Description
-//        -product type
-//        -upload date
-//        -tags
-//        -vendor
-//        -id
-//        -number of variants
-        let description = product.body_html ?? ""
-        let productType = product.productType ?? ""
-        let uploadDate = product.updated_at ?? ""
-        let tags = product.tags ?? ""
-//        let vendor = product.vendor ?? ""
-//        let id = String(product.id ??  -1)
-//        let numofVariants = product.variants?.count ?? 0
-        return description + productType + "\n" + uploadDate + "\n" + tags
- 
+    private func text() -> NSMutableAttributedString{
+        let text = NSMutableAttributedString (string: "")
+        //TO DO: ADD TOTAL QUANTITY
+       
+        guard let boldFont = UIFont(name: "HelveticaNeue-Bold", size: 17) else {return text}
+        guard let regularFont = UIFont(name: "HelveticaNeue", size: 15 ) else {return text}
+        
+        let subtitle = ["Descrption: ","Product Type: ", "Upload Date: ", "Tags: ","Vendor: ","ID: ","Number of Variants: "]
+        let information = [product.body_html ,product.productType ,product.updated_at ,product.tags,product.vendor  ,String(product.id ??  -1),String(product.variants?.count ?? 0)]
+        if subtitle.count != information.count {return text}
+        for (index,_) in subtitle.enumerated() {
+            let currentBoldString = NSMutableAttributedString(string: subtitle[index], attributes: [NSAttributedStringKey.font:boldFont, NSAttributedStringKey.foregroundColor:UIColor.white])
+            text.append(currentBoldString)
+
+            if let currentInfo = information[index] {
+                let currentRegString = NSMutableAttributedString(string: currentInfo+"\n\n\n", attributes: [NSAttributedStringKey.font:regularFont, NSAttributedStringKey.foregroundColor:UIColor.white])
+                text.append(currentRegString)
+            }
+            else{
+                text.append(NSAttributedString(string: "\n\n\n"))
+            }
+            
+            
+        }
+       
+        return text
     }
-    
+
+    @IBAction func onProductInfoViewPanGesture(_ sender: UIPanGestureRecognizer) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    private func toggleProductInfoViewConstraints(disableConstraints : Bool){
+        if (disableConstraints){
+            productInfoViewEqualHeightToSuperView.isActive = false
+            productInfoViewCenterToSafeAreaY.isActive = false
+            productInfoViewCenterToSafeAreaX.isActive = false
+            productInfoViewEqualWidthToSuperView.isActive = false
+        }
+        else{
+            self.productInfoViewEqualHeightToSuperView.isActive = true
+            self.productInfoViewCenterToSafeAreaY.isActive = true
+            self.productInfoViewCenterToSafeAreaX.isActive = true
+            self.productInfoViewEqualWidthToSuperView.isActive = true
+        }
+        self.productInformationView.layoutIfNeeded()
+    }
     
     
 }
--- Easify/ProductsCollectionViewController.swift
+++ Easify/ProductsCollectionViewController.swift
@@ -19,7 +19,6 @@ class ProductsCollectionViewController : UICollectionViewController {
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "productCell", for: indexPath) as! ProductCollectionViewCell
-        cell.layer.masksToBounds = true
         cell.layer.cornerRadius = 6
         if let imageURL = URL(string: products[indexPath.row].image?.src ?? "") {
             let downloadImageTask = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
@@ -41,7 +40,21 @@ class ProductsCollectionViewController : UICollectionViewController {
         let currentCell = collectionView.cellForItem(at: indexPath) as! ProductCollectionViewCell
         destinationVC.productImage = currentCell.productImageView.image
         destinationVC.product = products[indexPath.row]
-        self.navigationController?.pushViewController(destinationVC, animated: false)
+        //currentCell.isHidden = true
+        print("Cell after moved", currentCell.center.y)
         
+        UIView.animate(withDuration: 0.05, animations: {
+            currentCell.transform = CGAffineTransform(scaleX: 0.98, y: 0.98)
+            currentCell.transform = CGAffineTransform(translationX: 0, y: -3)
+
+        }) { (_) in
+          
+
+            destinationVC.cell = currentCell
+
+            self.navigationController?.present(destinationVC, animated: false)
+
+        }
+
     }
 }
